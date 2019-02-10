d7d361da4f0afc85ba01cd2660277f7f50d41c07
mokunshao <1627510957@qq.com>
2019-02-11 00:15:42 +0800

    备份

---
+++ src/modules/js/api.js
@@ -0,0 +1,11 @@
+export let url = {
+  hotLists: "/index/hotLists"
+};
+
+let host = "http://rap2api.taobao.org/app/mock/7058";
+
+for (let key in url) {
+  if (url.hasOwnProperty(key)) {
+    url[key] = host + url[key];
+  }
+}
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,10 +1,11 @@
-// import Vue from 'vue'
-// import App from './App.vue'
-import '../../modules/css/common.css';
-import './index.css';
+import Vue from "vue";
+import App from './App.vue'
+import "../../modules/css/common.css";
+import "../../modules/js/api.js";
+import "./index.css";
 
-// Vue.config.productionTip = false
+Vue.config.productionTip = false
 
-// new Vue({
-//   render: h => h(App),
-// }).$mount('#app')
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
