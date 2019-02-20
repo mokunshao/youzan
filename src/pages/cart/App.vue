0d824ecaa584f5cc017788a77c86d14b265f4d1d
mokunshao <1627510957@qq.com>
2019-02-20 17:38:52 +0800

    Update App.vue

--- src/pages/cart/App.vue
+++ src/pages/cart/App.vue
@@ -8,8 +8,9 @@
               <div class="block block-order block-cart" v-for="shop in cartList" :key="shop.shopId"
               >
                 <div class="header">
-                  <div class="select-group js-select-group">
-                    <span class="check" :class="{checked:shop.checked}" @click="selectShop(shop)"></span>
+                  <div class="select-group js-select-group" @click="selectShop(shop)">
+                    <span class="check" :class="{checked:shop.checked}" v-show="!editingShop"></span>
+                    <span class="check" :class="{checked:shop.removeChecked}" v-show="editingShop"></span>
                   </div>
                   <a class="shop-title">
                     <i class="custom-store-img">店铺：</i>
@@ -33,8 +34,8 @@
                       :class="{editing:shop.editing}"
                     >
                       <div>
-                        <div class="check-container">
-                          <span class="check" :class="{checked:good.checked}" @click="selectGood(shop,good)"></span>
+                        <div class="check-container" @click="selectGood(shop,good)">
+                          <span class="check" :class="{checked:editingShop? good.removeChecked:good.checked}"></span>
                         </div>
                         <div class="name-card clearfix">
                           <a
@@ -62,11 +63,11 @@
                             <div class="num" v-show="shop.editing">
                               <!---->
                               <div class="quantity">
-                                <button type="button" class="minus disabled"></button>
+                                <button type="button" class="minus" :class="{disabled:good.num===1}"></button>
                                 <input type="text" pattern="[0-9]*" class="txt" v-model="good.num">
                                 <button type="button" class="plus"></button>
-                                <div class="response-area response-area-minus"></div>
-                                <div class="response-area response-area-plus"></div>
+                                <div class="response-area response-area-minus" @click="reduce(good)"></div>
+                                <div class="response-area response-area-plus" @click="add(good)"></div>
                               </div>
                             </div>
                             <div class="price c-orange">
@@ -76,7 +77,7 @@
                           </div>
                           <div class="error-box"></div>
                         </div>
-                        <div class="delete-btn">
+                        <div class="delete-btn" @click="remove(shop,good)">
                           <span>删除</span>
                         </div>
                       </div>
@@ -136,12 +137,12 @@
               </div>
             </div>
             <div style="padding:0;" class="js-bottom-opts bottom-fix">
-              <div class="go-shop-tip js-go-shop-tip c-orange font-size-12">你需要分开结算每个店铺的商品哦~</div>
+              <!--<div class="go-shop-tip js-go-shop-tip c-orange font-size-12">你需要分开结算每个店铺的商品哦~</div>-->
               <div class="bottom-cart clear-fix">
                 <div class="select-all" @click="selectAll">
-                  <span class="check" :class="{checked:allSelect}"></span> 全选
+                  <span class="check" :class="{checked:editingShop? allRemoveSelected :allSelected}"></span> 全选
                 </div>
-                <!-- 显示状态 -->
+                <!-- 显示状态 -->return true
                 <div class="total-price" v-show="!editingShop">
                   合计：¥
                   <span class="js-total-price" style="color: rgb(255, 102, 0);">{{totalPrice}}</span>
@@ -169,14 +170,29 @@
         </div>
       </div>
     </div>
-    <div class="footer" style="min-height: 86px;">
-      <div class="copyright">
-        <div
-          class="ft-copyright"
-          style="background-image: url(https://img.yzcdn.cn/upload_files/2016/11/15/147918630760182374.png);"
-        >
-          <a class="yz-logo" href="https://www.youzan.com/?from_source=support_logo">有赞提供技术支持</a>
-        </div>
+    <!--<div class="footer" style="min-height: 86px;">-->
+      <!--<div class="copyright">-->
+        <!--<div-->
+          <!--class="ft-copyright"-->
+          <!--style="background-image: url(https://img.yzcdn.cn/upload_files/2016/11/15/147918630760182374.png);"-->
+        <!--&gt;-->
+          <!--<a class="yz-logo" href="https://www.youzan.com/?from_source=support_logo">有赞提供技术支持</a>-->
+        <!--</div>-->
+      <!--</div>-->
+    <!--</div>-->
+    <div id="0SuefKPV4p" v-show="showRemovePopup"
+         style="height: 100%; position: fixed; top: 0px; left: 0px; right: 0px; background-color: rgba(0, 0, 0, 0.7); z-index: 1000;  opacity: 1;"></div>
+    <div id="d2tfiX27YS" v-show="showRemovePopup" class="popout-confirm popout-box"
+         style="overflow: hidden; position: absolute; z-index: 1000; top: 50%; left: 50%; transform: translate3d(-50%, -50%, 0px); visibility: visible; border-radius: 4px; background: white; width: 270px; padding: 15px; opacity: 1;">
+      <div class="confirm-content font-size-14" style="line-height: 20px; padding: 5px 5px 10px;">
+        你确定要删除吗？
+      </div>
+      <hr style="margin: 9px -15px 10px;">
+      <div class="btn-2-1" style="width: 47.5%;">
+        <p class="js-cancel center font-size-16" style="padding-top: 5px;" @click="showRemovePopup=false">取消</p>
+      </div>
+      <div class="btn-2-1" style="width: 47.5%;">
+        <p class="js-ok center c-green font-size-16" style="padding-top: 5px;" @click="confirmRemove">确定</p>
       </div>
     </div>
   </div>
@@ -193,7 +209,9 @@
     data() {
       return {
         cartList: null,
-        editingShop: null
+        editingShop: null,
+        showRemovePopup: false,
+        willRemoveData: null
       };
     },
     mounted() {
@@ -221,35 +239,66 @@
           });
       },
       selectGood(shop, good) {
-        good.checked = !good.checked;
-        shop.checked = shop.goodsList.every((good) => {
-          return good.checked;
+        let attr = this.editingShop ? 'removeChecked' : 'checked';
+        good[attr] = !good[attr];
+        shop[attr] = shop.goodsList.every((good) => {
+          return good[attr];
         });
       },
       selectShop(shop) {
-        shop.checked = !shop.checked;
+        let attr = this.editingShop ? 'removeChecked' : 'checked';
+        shop[attr] = !shop[attr];
         shop.goodsList.forEach(good => {
-          good.checked = shop.checked;
+          good[attr] = shop[attr];
         });
       },
       selectAll() {
-        this.allSelect = !this.allSelect;
+        let attr = this.editingShop ? 'allRemoveSelected' : 'allSelected';
+        this[attr] = !this[attr];
       },
       edit(shop) {
         shop.editing = !shop.editing;
         shop.editingMsg = shop.editing === true ? '完成' : '编辑';
         this.cartList.forEach((item) => {
           if (shop.shopId !== item.shopId) {
-            // console.table(item.shopTitle)
             item.editing = false;
             item.editingMsg = shop.editing ? '' : '编辑';
           }
         });
-        this.editingShop = shop.editing ? shop : null;
+        this.editingShop = shop;
+      },
+      reduce(good) {
+        if (good.num === 1) return;
+        axios.post('https://nei.netease.com/api/apimock/dd43479bc45ee7491c66cc246d9c46b8/cart/good/reduce', {id: good.id}).then(() => {
+          good.num--;
+        });
+      },
+      add(good) {
+        axios.post('https://nei.netease.com/api/apimock/dd43479bc45ee7491c66cc246d9c46b8/cart/good/add', {id: good.id}).then(() => {
+          good.num++;
+        });
+      },
+      remove(shop, good) {
+        this.showRemovePopup = true;
+        this.willRemoveData = {shop, good};
+      },
+      confirmRemove() {
+        axios.post('https://nei.netease.com/api/apimock/dd43479bc45ee7491c66cc246d9c46b8/cart/good/remove', this.willRemoveData).then(() => {
+          this.willRemoveData.shop.goodsList.splice(this.willRemoveData.shop.goodsList.indexOf(this.willRemoveData.good), 1);
+          if (!this.willRemoveData.shop.goodsList.length) {
+            this.cartList.splice(this.cartList.indexOf(this.willRemoveData.shop), 1);
+            this.editingShop = null;
+            this.cartList.forEach(shop => {
+              shop.editingMsg = '编辑';
+            });
+          }
+          this.showRemovePopup = false;
+
+        });
       }
     },
     computed: {
-      allSelect: {
+      allSelected: {
         get() {
           if (this.cartList) {
             return this.cartList.every(shop => {
@@ -292,14 +341,33 @@
         }
         return 0;
       },
-      removeLists() {
-
+      willRemoveLists() {
+        if (this.editingShop) {
+          let arr = [];
+          this.editingShop.goodsList.forEach(good => {
+            if (good.removeChecked) {
+              arr.push(good);
+            }
+          });
+          return arr;
+        }
+        return [];
       },
-      allRemoveSelected:{
-        get(){
-
+      allRemoveSelected: {
+        get() {
+          if (this.editingShop) {
+            return this.editingShop.removeChecked;
+          }
+          return false;
         },
-        set(newVal){}
+        set(newVal) {
+          if (this.editingShop) {
+            this.editingShop.removeChecked = newVal;
+            this.editingShop.goodsList.forEach(good => {
+              good.removeChecked = newVal;
+            });
+          }
+        }
       }
     }
   };
